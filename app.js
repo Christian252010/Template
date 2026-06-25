46fe8ba2420c6f9b4f6f7640dc9670bfa19f3638
Christian252010 <146352144+Christian252010@users.noreply.github.com>
2026-06-25 13:32:55 +0700

    update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
