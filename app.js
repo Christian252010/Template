b19dc067148eb1a317945545b9786b199977c977
Christian252010 <146352144+Christian252010@users.noreply.github.com>
2026-06-25 13:34:55 +0700

    update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAk5vpwEms61MGUMHf42v-5l5YsCKZxPcU",
     authDomain: "music-e4d6a.firebaseapp.com",
-    projectId: "...",
-    storageBucket: "...",
+    projectId: "music-e4d6a",
+    storageBucket: "music-e4d6a.firebasestorage.app",
     messagingSenderId: "...",
     appId: "..."
 };
